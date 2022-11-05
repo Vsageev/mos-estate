50982bed5e3e52515101fb537e339137b176f318
SKR-X <skrillexazem1@bk.ru>
2022-11-05 14:23:21 +0300

    added login functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:mos_estate/pages/startup/startup_page.dart';
+import 'package:mos_estate/shared/services/login_service.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  LoginService.instance.token = prefs.getString('token');
+
   runApp(
     OverlaySupport(
       child: MaterialApp(
--- lib/pages/startup/startup_page.dart
+++ lib/pages/startup/startup_page.dart
@@ -4,18 +4,14 @@ import 'package:mos_estate/pages/import/import_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mos_estate/pages/login/login_cubit.dart';
 import 'package:mos_estate/pages/login/login_page.dart';
-import 'package:mos_estate/pages/standart_calculation/standart_calculation_cubit.dart';
-import 'package:mos_estate/pages/standart_calculation/standart_calculation_page.dart';
 import 'package:mos_estate/shared/services/login_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class StartupPage extends StatelessWidget {
   const StartupPage({super.key});
   
 
-  _navigate(BuildContext context) async {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      var token = prefs.getString('token');
+  _navigate(BuildContext context) {
+    var token = LoginService.instance.token;
     if (token == null) {
       Navigator.push(
         context,
